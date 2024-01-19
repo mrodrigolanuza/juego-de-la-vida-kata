c9683d46a8b6b30eba3bf07185254374f3ff6c38
Marcos Rodrigo <marcosrodrigolanuza@gmail.com>
2024-01-19 21:17:03 +0100

    Refactor: Create cell creation tests.

--- src/tests/cell.test.ts
+++ src/tests/cell.test.ts
@@ -2,7 +2,21 @@ import { Cell } from '../core/cell';
 
 describe('The Cell', ()=>{
 
-	 it('should be asked about its state of life.', ()=>{
+	it('should be created alive.', ()=>{
+
+		let cell = Cell.createAlive();
+		
+        expect(cell.isAlive()).toBe(true); 
+     }); 
+
+     it('should be created dead.', ()=>{
+
+		let cell = Cell.createDead();
+		
+        expect(cell.isAlive()).toBe(false); 
+     }); 
+    
+    it('should be asked about its state of life.', ()=>{
 
 		let cell = Cell.createAlive();
 		
