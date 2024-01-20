f01e6943079fd0a8b8b2dde2d584d1feab657735
Marcos Rodrigo <marcosrodrigolanuza@gmail.com>
2024-01-20 19:31:26 +0100

    Green: when a living cell has less than two living neighbours then the cell is dead by underpopulation.

--- src/core/world.ts
+++ src/core/world.ts
@@ -4,28 +4,54 @@ export class World {
 	private world: Cell[][] = [];
 
 	private constructor(rows: number, cols: number) {
-		this.createWorldWithDeadCells(rows, cols);
+		this.world = this.createWorldWithDeadCells(rows, cols);
+	}
+    
+	static create(rows: number, cols: number): World {
+        if (rows <= 0) throw Error('Number of rows should be greater than zero.');
+        
+		if (cols <= 0) throw Error('Number of columns should be greater than zero.');
+        
+		return new World(rows, cols);
 	}
 
-    private createWorldWithDeadCells(rows: number, cols: number) {
+	setLivingCell(row: number, col: number){
+        this.world[row][col].setLivingState();
+    }
+    
+    currentGeneration() {
+        return this.world;
+	}
+
+    nextGeneration() {
+        let newGenWorld = this.createWorldWithDeadCells(this.world.length, this.world[0].length);
+
+        for (let row = 0; row < this.world.length; row++) {
+            for (let col = 0; col < this.world[0].length; col++) {
+                const totalLivingNeighbour = this.numberLivingNeighboursOfCell(row, col);
+                newGenWorld[row][col].setDeadState();
+                if((totalLivingNeighbour == 2)||(totalLivingNeighbour == 3)){
+                    newGenWorld[row][col].setLivingState();
+                }
+            }
+        }
+        
+        return newGenWorld;
+	}
+
+    private createWorldWithDeadCells(rows: number, cols: number): Cell[][] {
+        let newWorld: Cell[][] = [];
         for (let row = 0; row < rows; row++) {
             const row: Cell[] = [];
             for (let col = 0; col < cols; col++) {
                 row.push(Cell.createDead());
             }
-            this.world.push(row);
+            newWorld.push(row);
         }
+        return newWorld;
     }
 
-	static create(rows: number, cols: number): World {
-		if (rows <= 0) throw Error('Number of rows should be greater than zero.');
-
-		if (cols <= 0) throw Error('Number of columns should be greater than zero.');
-
-		return new World(rows, cols);
-	}
-
-	nextGeneration() {
-		return this.world;
-	}
+    private numberLivingNeighboursOfCell(row: number, col:number): number{
+        return 0;
+    }
 }
--- src/tests/world.test.ts
+++ src/tests/world.test.ts
@@ -36,17 +36,44 @@ describe('The World', () => {
 		}).toThrow(Error);
 	});
 
-	it('should create 1x1 world with only a dead cell.', () => {
+	it('should create initial 1x1 world with only a dead cell.', () => {
 		const ROWS: number = 1;
 		const COLS: number = 1;
 
 		const world = World.create(ROWS, COLS);
-		const nextGen = world.nextGeneration();
+		const actualGen = world.currentGeneration();
 
-		expect(nextGen.length).toBe(1);
-		expect(nextGen[0].length).toBe(1);
-		expect(nextGen[0][0]).toBeInstanceOf(Cell);
-		expect(nextGen[0][0].isAlive()).toBe(false);
+		expect(actualGen.length).toBe(1);
+		expect(actualGen[0].length).toBe(1);
+		expect(actualGen[0][0]).toBeInstanceOf(Cell);
+		expect(actualGen[0][0].isAlive()).toBe(false);
+	});
+
+    it('should create initial 1x1 world with only a living cell.', () => {
+		const ROWS: number = 1;
+		const COLS: number = 1;
+
+		const world = World.create(ROWS, COLS);
+        const actualGen = world.currentGeneration();
+        world.setLivingCell(0,0);
+
+		expect(actualGen.length).toBe(1);
+		expect(actualGen[0].length).toBe(1);
+		expect(actualGen[0][0]).toBeInstanceOf(Cell);
+        expect(actualGen[0][0].isAlive()).toBe(true);
+	});
+
+    it('should create initial 4x8 world with only a living cell.', () => {
+		const ROWS: number = 4;
+		const COLS: number = 8;
+
+		const world = World.create(ROWS, COLS);
+        const actualGen = world.currentGeneration();
+        world.setLivingCell(1,4);
+
+		expect(actualGen.length).toBe(4);
+		expect(actualGen[0].length).toBe(8);
+        expect(actualGen[1][4].isAlive()).toBe(true);
 	});
 
     it('when a living cell has less than two living neighbours then the cell is dead by underpopulation.', () => {
