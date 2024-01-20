13cdb3fdde9a48f051e91818222df2e2b3814ae9
Marcos Rodrigo <marcosrodrigolanuza@gmail.com>
2024-01-20 18:20:25 +0100

    Refactor: extract method when building initial world

--- src/core/world.ts
+++ src/core/world.ts
@@ -4,14 +4,18 @@ export class World {
 	private world: Cell[][] = [];
 
 	private constructor(rows: number, cols: number) {
-		for (let row = 0; row < rows; row++) {
-			const row: Cell[] = [];
-			for (let col = 0; col < cols; col++) {
-				row.push(Cell.createDead());
-			}
-			this.world.push(row);
-		}
+		this.createWorldWithDeadCells(rows, cols);
 	}
+
+    private createWorldWithDeadCells(rows: number, cols: number) {
+        for (let row = 0; row < rows; row++) {
+            const row: Cell[] = [];
+            for (let col = 0; col < cols; col++) {
+                row.push(Cell.createDead());
+            }
+            this.world.push(row);
+        }
+    }
 
 	static create(rows: number, cols: number): World {
 		if (rows <= 0) throw Error('Number of rows should be greater than zero.');
