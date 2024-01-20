7490c8d8b77544ef2e20b1cb7a760a4345f4e2d1
Marcos Rodrigo <marcosrodrigolanuza@gmail.com>
2024-01-21 00:19:37 +0100

    Refactor: modify expressiveness of some methods

--- src/core/world.ts
+++ src/core/world.ts
@@ -45,129 +45,128 @@ export class World {
     }
 
 	private hasDeadCellThreeLivingNeighbours(row: number, col: number): boolean {
-		const totalLivingNeighbour = this.numberLivingNeighboursOfCell(row, col);
-		if (!this.world[row][col].isAlive() && totalLivingNeighbour == 3) {
-			return true;
-		}
-		return false;
+        const ALIVE_NEIGHBOURS_FOR_REVIVING = 3;
+		return this.isDeadCell(row, col) && this.numberLivingNeighboursOfCell(row, col) == ALIVE_NEIGHBOURS_FOR_REVIVING;
 	}
 
+    
 	private hasLivingCellSuitableRangeOfLivingNeighbours(row: number, col: number): boolean {
-		const totalLivingNeighbour = this.numberLivingNeighboursOfCell(row, col);
-		if (this.world[row][col].isAlive() && (totalLivingNeighbour == 2 || totalLivingNeighbour == 3)) {
-			return true;
-		}
-		return false;
+        const totalLivingNeighbour = this.numberLivingNeighboursOfCell(row, col);
+		return this.world[row][col].isAlive() && (totalLivingNeighbour == 2 || totalLivingNeighbour == 3);
 	}
-
+    
 	private numberLivingNeighboursOfCell(row: number, col: number): number {
-		let livingNeighbours: number = 0;
-
+        let livingNeighbours: number = 0;
+        
 		if (this.existUpperLeftNeighbour(row, col) && this.isLivingUpperLeftCell(row, col)) {
-			livingNeighbours++;
+            livingNeighbours++;
 		}
-
+        
 		if (this.existUpperNeighbour(row) && this.isLivingUpperCell(row, col)) {
-			livingNeighbours++;
+            livingNeighbours++;
 		}
-
+        
 		if (this.existUpperRightNeighbour(row, col) && this.isLivingUpperRightCell(row, col)) {
-			livingNeighbours++;
+            livingNeighbours++;
 		}
-
+        
 		if (this.existRightNeighbour(col) && this.isLivingRightCell(row, col)) {
-			livingNeighbours++;
+            livingNeighbours++;
 		}
-
+        
 		if (this.existLowerRightNeighbour(row, col) && this.isLivingLowerRightCell(row, col)) {
-			livingNeighbours++;
+            livingNeighbours++;
 		}
-
+        
 		if (this.existLowerNeighbour(row) && this.isLivingLowerCell(row, col)) {
 			livingNeighbours++;
 		}
-
+        
 		if (this.existLowerLeftNeighbour(row, col) && this.isLivingLowerLeftCell(row, col)) {
-			livingNeighbours++;
+            livingNeighbours++;
 		}
-
+        
 		if (this.existLeftNeighbour(col) && this.isLivingLeftCell(row, col)) {
-			livingNeighbours++;
+            livingNeighbours++;
 		}
-
+        
 		return livingNeighbours;
 	}
-
+    
 	private isLivingLeftCell(row: number, col: number) {
-		return this.world[row][col - 1].isAlive();
+        return this.world[row][col - 1].isAlive();
 	}
-
+    
 	private existLeftNeighbour(col: number) {
-		return col - 1 >= 0;
+        return col - 1 >= 0;
 	}
-
+    
 	private isLivingLowerLeftCell(row: number, col: number) {
-		return this.world[row + 1][col - 1].isAlive();
+        return this.world[row + 1][col - 1].isAlive();
 	}
-
+    
 	private existLowerLeftNeighbour(row: number, col: number) {
-		return row + 1 < this.ROWS && col - 1 >= 0;
+        return row + 1 < this.ROWS && col - 1 >= 0;
 	}
-
+    
 	private isLivingLowerCell(row: number, col: number) {
-		return this.world[row + 1][col].isAlive();
+        return this.world[row + 1][col].isAlive();
 	}
-
+    
 	private existLowerNeighbour(row: number) {
-		return row + 1 < this.ROWS;
+        return row + 1 < this.ROWS;
 	}
-
+    
 	private isLivingLowerRightCell(row: number, col: number) {
-		return this.world[row + 1][col + 1].isAlive();
+        return this.world[row + 1][col + 1].isAlive();
 	}
-
+    
 	private existLowerRightNeighbour(row: number, col: number) {
-		return row + 1 < this.ROWS && col + 1 < this.COLS;
+        return row + 1 < this.ROWS && col + 1 < this.COLS;
 	}
-
+    
 	private isLivingRightCell(row: number, col: number) {
-		return this.world[row][col + 1].isAlive();
+        return this.world[row][col + 1].isAlive();
 	}
-
+    
 	private existRightNeighbour(col: number) {
-		return col + 1 < this.COLS;
+        return col + 1 < this.COLS;
 	}
-
+    
 	private isLivingUpperRightCell(row: number, col: number) {
-		return this.world[row - 1][col + 1].isAlive();
+        return this.world[row - 1][col + 1].isAlive();
 	}
-
+    
 	private existUpperRightNeighbour(row: number, col: number) {
-		return row - 1 >= 0 && col + 1 < this.COLS;
+        return row - 1 >= 0 && col + 1 < this.COLS;
 	}
-
+    
 	private isLivingUpperCell(row: number, col: number) {
-		return this.world[row - 1][col].isAlive();
+        return this.world[row - 1][col].isAlive();
 	}
-
+    
 	private existUpperNeighbour(row: number) {
-		return row - 1 >= 0;
+        return row - 1 >= 0;
 	}
-
+    
 	private isLivingUpperLeftCell(row: number, col: number) {
-		return this.world[row - 1][col - 1].isAlive();
+        return this.world[row - 1][col - 1].isAlive();
 	}
-
+    
 	private existUpperLeftNeighbour(row: number, col: number) {
-		return row - 1 >= 0 && col - 1 >= 0;
+        return row - 1 >= 0 && col - 1 >= 0;
 	}
-
+    
+    private isDeadCell(row: number, col: number) {
+        return !this.world[row][col].isAlive();
+    }
+    
 	private createWorldWithDeadCells(rows: number, cols: number): Cell[][] {
-		const newWorld: Cell[][] = [];
+        const newWorld: Cell[][] = [];
 		for (let row = 0; row < rows; row++) {
-			const row: Cell[] = [];
+            const row: Cell[] = [];
 			for (let col = 0; col < cols; col++) {
-				row.push(Cell.createDead());
+                row.push(Cell.createDead());
 			}
 			newWorld.push(row);
 		}
